f8ffc588ea359ebb86067f7f693f6960c19a78ee
dominikuu <alanccl92@hotmail.com>
2022-10-18 10:08:34 +0800

    Add .gitignore

--- backend/postgres/seed/seed.sql
+++
@@ -1,3 +0,0 @@
--- Seed data with a fake user for testing
-
-COPY salary(timestamp, employer, job_title, year_employer, year_exp, base_pay, signing_bonus, annaul_bonus, annual_stock, sex) FROM '/docker-entrypoint-initdb.d/seed/dummy.csv' DELIMITER ',' CSV HEADER
